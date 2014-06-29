530afff1287b8c2689026b1dfb0ad932f5f10da6
Matthew Szenher <mszenher@mdsol.com>
2014-06-29 14:12:11 -0400

    consolidate spec

--- spec/delayed_job_sqs_spec.rb
+++ spec/delayed_job_sqs_spec.rb
@@ -28,4 +28,25 @@ end
 
 describe Delayed::Backend::Sqs::Job, :sqs do
   it_behaves_like 'a delayed_job backend'
+  
+  let(:simple_job) { DelayedJobSqs::SimpleJob.new }
+  
+  it 'delays a single simple job successfully' do
+    before_runs_count = DelayedJobSqs::SimpleJob.runs
+    
+    simple_job.delay.perform
+    Delayed::Worker.new.work_off
+    
+    DelayedJobSqs::SimpleJob.runs.should == (before_runs_count + 1)
+  end
+  
+  it 'delays multiple simple jobs successfully' do
+    before_runs_count = DelayedJobSqs::SimpleJob.runs
+    
+    n = 2
+    n.times{ simple_job.delay.perform }
+    Delayed::Worker.new.work_off
+    
+    DelayedJobSqs::SimpleJob.runs.should == (before_runs_count + n)
+  end
 end
