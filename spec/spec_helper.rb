a260ae91c2f45f15b52b599c3b50add32a31d027
Matthew Szenher <mszenher@mdsol.com>
2014-06-30 14:11:42 -0400

    add comments about rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,10 @@ require 'aws-sdk'
 
 Dir["#{SPEC_DIR}/support/*.rb"].each { |f| require f }
 
-QUEUE_NAME = 'default'
+QUEUE_NAME = 'default' # A queue name to be used by default by both delayed_jobs and delayed_jobs_sqs.
+
+# Define AWS config. to be used in tests.  This is for the benefit of telling delayed_job_sqs where the sqs endpoint is
+# and what credentials to use to talk to it.  Here, we use localhost b/c we are using fake_sqs as our SQS endpoint.
 AWS.config(
   use_ssl:            false,
   sqs_endpoint:       'localhost',
@@ -35,6 +38,7 @@ AWS.config(
 
 require File.join(LIB_DIR, 'delayed_job_sqs')
 
+# Tell DJ where to log.  These logs are useful for debugging purposes.
 Delayed::Worker.logger = Logger.new('/tmp/dj.log')
 
 # TODO:  Shouldn't DJ_SQS just set the queue name(s)?
@@ -44,16 +48,19 @@ RSpec.configure do |config|
   config.mock_with :rspec
   config.treat_symbols_as_metadata_keys_with_true_values = true
   
+  # Before running the test suite, initialize and start fake_sqs.
   config.before(:suite) do 
     $fake_sqs = FakeSQS::TestIntegration.new(database: ':memory:')
     $fake_sqs.start
     $sqs = AWS::SQS.new
   end
   
+  # Each example tagged with :sqs, we reset fake_sqs and recreate the SQS queue in which we store jobs.
   config.before(:each, :sqs) do
     $fake_sqs.reset
     $sqs.queues.create(QUEUE_NAME)
   end
   
+  # After running the test suite, stop fake_sqs.
   config.after(:suite) { $fake_sqs.stop }
 end
