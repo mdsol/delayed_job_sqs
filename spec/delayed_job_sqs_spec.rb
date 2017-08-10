72bd2565aca29321469ad908c86ac482a3efc1da
Edward J. Jinotti <ejinotti@mdsol.com>
2017-08-10 18:55:27 -0400

    Begin spec fixin efforts.

--- spec/delayed_job_sqs_spec.rb
+++ spec/delayed_job_sqs_spec.rb
@@ -4,13 +4,13 @@ require 'active_record'
 require 'fake_sqs'
 
 # This story class is a simple class required by the shared specs.  It is in fact copied from DJ repo.
-# The shared specs expect the Story class to have a lot of ActiveRecord-like qualities so I'm just 
+# The shared specs expect the Story class to have a lot of ActiveRecord-like qualities so I'm just
 # making it an ActiveRecord-based class.
 # Used to test interactions between DJ and an ORM
 ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => ':memory:'
 ActiveRecord::Base.logger = Delayed::Worker.logger
 ActiveRecord::Migration.verbose = false
-  
+
 ActiveRecord::Schema.define do
   create_table :stories, :primary_key => :story_id, :force => true do |table|
     table.string :text
@@ -29,62 +29,66 @@ end
 
 describe Delayed::Backend::Sqs::Job, :sqs do
   it_behaves_like 'a delayed_job backend'
-  
+
   let(:simple_job) { DelayedJobSqs::SimpleJob.new }
-  
-  let(:sqs_message) { AWS::SQS::ReceivedMessage.new(AWS::SQS::Queue.new("http://0.0.0.0:#{AWS.config.sqs_port}/#{DEFAULT_QUEUE_NAME}"),
-   1, "", opts = {body: {job: "New job"}.to_json}) }
-  
+
+  let(:queue_url) { "http://0.0.0.0:#{AWS.config['sqs_port']}" }
+  let(:sqs_queue) { Aws::SQS::Queue.new(queue_url) }
+  let(:sqs_message) { Aws::SQS::Message.new(sqs_queue.url, 1, '', body: { job: 'New job' }.to_json) }
+
+  # let(:sqs_message) { AWS::SQS::ReceivedMessage.new(AWS::SQS::Queue.new("http://0.0.0.0:#{AWS.config.sqs_port}/#{DEFAULT_QUEUE_NAME}"),
+  #  1, "", opts = {body: {job: "New job"}.to_json}) }
+
   let(:sqs_job) { Delayed::Backend::Sqs::Job.new(sqs_message) }
-  
+
   [1, 2].each do |num_jobs|
     it "delays #{num_jobs} simple job(s) successfully" do
       before_runs_count = DelayedJobSqs::SimpleJob.runs
-    
+
       num_jobs.times{ simple_job.delay.perform }
       Delayed::Worker.new.work_off
-    
+
       DelayedJobSqs::SimpleJob.runs.should == (before_runs_count + num_jobs)
     end
   end
-  
+
   after do
     $fake_sqs.start
     $fake_sqs.clear_failure
   end
-  
+
   describe 'enqueue' do
 
     it 'raises if AWS SQS returns non ok status' do
       $fake_sqs.api_fail('send_message')
       expect {described_class.enqueue(payload_object: SimpleJob.new)}.to raise_error(FakeSQS::InvalidAction)
     end
-    
+
     it 'raises if AWS SQS fails to respond' do
       $fake_sqs.stop
       expect {described_class.enqueue(payload_object: SimpleJob.new)}.to raise_error(Errno::ECONNREFUSED)
     end
 
   end
-  
+
   describe 'fail' do
-    
+
     context 'with sqs message' do
-      
+
       after do
         sqs_job.fail!
       end
-      
+
       it 'destroys the job' do
         sqs_message.should_receive(:delete)
       end
-    
+
       it 'logs the failure' do
         sqs_job.should_receive(:puts).with(/Job with attributes/)
         sqs_job.should_receive(:puts).with(/Job destroyed!/)
       end
     end
-    
+
     it 'logs the failure to destroy the job without sqs message' do
       sqs_job_no_msg = Delayed::Backend::Sqs::Job.new
       sqs_job_no_msg.should_receive(:puts).with(/Job with attributes/)
@@ -93,14 +97,14 @@ describe Delayed::Backend::Sqs::Job, :sqs do
     end
 
   end
-  
+
   describe 'retry' do
-    
+
     it 'does not delete the job if failed to resend it' do
       $fake_sqs.api_fail('send_message')
       sqs_message.should_not_receive(:delete)
-      expect { sqs_job.save }.to raise_error(AWS::SQS::Errors::InvalidAction)
+      expect { sqs_job.save }.to raise_error(Aws::SQS::ServiceError)
     end
   end
-  
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require 'delayed_job'
 Bundler.setup
 
 require 'fake_sqs/test_integration'
-require 'aws-sdk-v1'
+require 'aws-sdk'
 
 Dir["#{SPEC_DIR}/support/*.rb"].each { |f| require f }
 
@@ -26,13 +26,12 @@ DEFAULT_QUEUE_NAME = 'default' # A queue name to be used by default by both dela
 
 # Define AWS config. to be used in tests.  This is for the benefit of telling delayed_job_sqs where the sqs endpoint is
 # and what credentials to use to talk to it.  Here, we use localhost b/c we are using fake_sqs as our SQS endpoint.
-AWS.config(
-  use_ssl:            false,
-  sqs_endpoint:       'localhost',
-  sqs_port:           4568,
-  access_key_id:      'fake',
-  secret_access_key:  'fake',
-  sqs_queue_name:     DEFAULT_QUEUE_NAME,
+Aws.config.update(
+  sqs: {
+    endpoint: 'localhost',
+    port: 4568,
+    queue_name: DEFAULT_QUEUE_NAME,
+  },
 )
 
 require File.join(LIB_DIR, 'delayed_job_sqs')
@@ -40,29 +39,29 @@ require File.join(LIB_DIR, 'delayed_job_sqs')
 # Queue names for queues used by dj_sqs specific tests as well as the dj backend shared examples ('a delayed_job backend').
 QUEUES_TO_CREATE = [DEFAULT_QUEUE_NAME, 'tracking', 'small', 'medium', 'large', 'one', 'two']
 
-RSpec.configure do |config|  
+RSpec.configure do |config|
   config.mock_with :rspec
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  
+
   # Before running the test suite, initialize and start fake_sqs.
-  config.before(:suite) do 
+  config.before(:suite) do
     $fake_sqs = FakeSQS::TestIntegration.new(database: ':memory:')
     $fake_sqs.start
-    $sqs = AWS::SQS.new
+    $sqs = Aws::SQS::Resource.new
   end
-  
+
   # Each example tagged with :sqs, we reset fake_sqs and recreate the SQS queue in which we store jobs.
   config.before(:each, :sqs) do
     $fake_sqs.reset
-    QUEUES_TO_CREATE.each{ |q_name| $sqs.queues.create(q_name) }
+    QUEUES_TO_CREATE.each{ |q_name| $sqs.create_queue(queue_name: q_name) }
   end
-  
+
   config.before(:each) do
     Delayed::Worker.logger = Logger.new('/tmp/dj.log')
     # TODO:  Shouldn't DJ_SQS just set the queue name(s)?
     Delayed::Worker.queues = QUEUES_TO_CREATE
   end
-        
+
   # After running the test suite, stop fake_sqs.
   config.after(:suite) { $fake_sqs.stop }
 end
